36ee4d8c5b38e8d808cb04434b45afda6424d35d
rohitpanicker06 <rohitpanicker06@gmail.com>
2024-04-07 19:09:49 -0400

    Made changes for displaying NFT's

--- my-nft-app/src/MyNFTs.jsx
+++ my-nft-app/src/MyNFTs.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import NFTMarketplaceABI from './NFTMarketPlaceABI.json';
 import { useEthersSigner } from './getSigner'
 import { ethers } from 'ethers';
+import axios from 'axios'; 
 
 const contractAddress = "0xdc205b043cc5aBc33e5d7B71f9f888b2F0a7A020"
 
@@ -22,15 +23,37 @@ function MyNFTs() {
       );
 
       const listedTokens = await contract.getMyNFTs();
-      const nftsWithURI = listedTokens.map(({ tokenId, owner, tokenURI }) => ({
-        tokenId,
-        owner,
-        tokenURI
+
+      // const nfts = await Promise.all(listedTokens.map(async i => {
+      //   const tokenURI = await contract.tokenURI(i.tokenId);
+      //   let meta = await axios.get(tokenURI);
+      //   meta = meta.data;
+
+      //   let nft = {
+      //     tokenId: i.tokenId.toNumber(),
+      //     image: meta.image,
+      //     description: meta.description,
+      //     fileName: meta.fileName
+
+      //   }
+      //   return nft;
+      // }));
+
+
+      const nfts = await Promise.all(listedTokens.map(async i => {
+        const imageURI = await contract.tokenURI(i.tokenId);
+      
+        let nft = {
+          tokenId: i.tokenId.toNumber(),
+          image: imageURI
+        }
+        return nft;
       }));
+      
 
-      console.log(nftsWithURI)
 
-      setNfts(nftsWithURI);
+      console.log(nfts)
+      setNfts(nfts);
     } catch (error) {
       console.error("Error fetching NFTs:", error);
     }
@@ -44,10 +67,10 @@ function MyNFTs() {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h2>My NFTs</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        {nfts.map(({ tokenId }, index) => (
+        {nfts.map(({ tokenId, image }, index) => (
           <div key={index} style={{ margin: '10px', width: '300px', textAlign: 'center' }}>
-            <img src={tokenId} alt={`NFT ${index}`} style={{ width: '100%', marginBottom: '10px' }} />
-            <p>Image URL: {tokenId}</p>
+            <img src={image} alt={`NFT ${index}`} style={{ width: '100%', marginBottom: '10px' }} />
+            <p>Image URL: {image}</p> {/* Display the correct image URL */}
           </div>
         ))}
       </div>
